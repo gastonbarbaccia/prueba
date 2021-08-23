6610f57043c42222cd730189ca7884e64e3dc712
gastonbarbaccia <gastonbarbaccia@hotmail.com>
2021-08-23 19:46:57 -0300

    Se agrega la conexion para base de datos remota

--- conexiondb.php
+++ conexiondb.php
@@ -2,10 +2,19 @@
 
 function conexion_db(){
 
-$basededatos = "dashhtml";
+//Base de datos local
+/* $basededatos = "dashhtml";
 $usuario = "root" ;
 $contrasena = "";
-$conexion = "localhost";
+$conexion = "localhost"; */
+
+
+//Hostinger Mysql
+$basededatos = "u463129590_prueba_gb";
+$usuario = "u463129590_gaston" ;
+$contrasena = "Ns2b7bfqbf!";
+$conexion = "sql234.main-hosting.eu";
+
 
 $cadena_conexion = new mysqli($conexion,$usuario,$contrasena,$basededatos);
 
